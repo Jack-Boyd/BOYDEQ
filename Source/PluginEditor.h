89a1cb09fd598725aa4d25304d44daaee8f1f676
Jack Boyd <jack.boyd@native-instruments.com>
2025-05-21 21:09:27 +0100

    create response curve and map peak filter to it

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,9 @@ struct CustomRotarySlider : juce::Slider
     }
 };
 
-class BOYDEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class BOYDEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
+juce::AudioProcessorParameter::Listener,
+juce::Timer
 {
 public:
     BOYDEQAudioProcessorEditor (BOYDEQAudioProcessor&);
@@ -32,11 +34,18 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override {}
+    
+    void timerCallback() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     BOYDEQAudioProcessor& audioProcessor;
+    
+    juce::Atomic<bool> parametersChanged { false };
     
     CustomRotarySlider peakFreqSlider,
     peakGainSlider,
@@ -58,6 +67,8 @@ private:
                 highCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
+    
+    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BOYDEQAudioProcessorEditor)
 };
